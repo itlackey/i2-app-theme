5be93e25bdffd046d31be0e8eddcda262b16829f
ITLackey <itlackey@gmail.com>
2019-12-18 10:31:54 -0600

    fixed issue when path specified with aspnet

--- utils/installer.js
+++ utils/installer.js
@@ -2,16 +2,6 @@ const { src, dest } = require('gulp');
 const replace = require('gulp-replace');
 const path = require('path');
 
-async function installTheme(config) {
-  const destPath = path.join(process.cwd(), config.path);
-
-  if (config.verbose) console.log(`Installing from $${__dirname}/dist/**/*.* to ${destPath}`);
-
-  await src(`${__dirname}/../dist/assets/**/*.*`).pipe(dest(path.join(destPath, 'assets')));
-  await src(`${__dirname}/../dist/index.html`)
-    .pipe(replace('{{site.title}}', config.name))
-    .pipe(dest(destPath));
-}
 
 async function installESLintConfig(target, verbose) {
   const destPath = path.join(process.cwd(), target);
@@ -21,13 +11,24 @@ async function installESLintConfig(target, verbose) {
   await src(`${__dirname}/../dist/.eslintrc.js`).pipe(dest(destPath));
 }
 
+async function installTheme(config) {
+  const destPath = path.join(process.cwd(), config.path);
+
+  if (config.verbose) console.log(`Installing from ${__dirname}/dist/**/*.* to ${destPath}`);
+
+  await src(`${__dirname}/../dist/assets/**/*.*`).pipe(dest(path.join(destPath, 'assets')));
+  await src(`${__dirname}/../dist/index.html`)
+    .pipe(replace('{{site.title}}', config.name))
+    .pipe(dest(destPath));
+}
+
 async function installAspnetAssets(config) {
-  await installESLintConfig('.', config.verbose);
-  await installTheme({ path: 'wwwroot/', verbose: config.verbose });
+  await installESLintConfig(config.path, config.verbose);
+  await installTheme({ path: path.join(config.path, 'wwwroot/'), verbose: config.verbose });
 
   const destPath = path.join(process.cwd(), config.path, 'Views');
 
-  if (config.verbose) console.log(`Installing aspnet assets from ${__dirname}/../dist/aspnet/ to ${destPath}`);
+  if (config.verbose) console.log(`Installing aspnet assets from ${__dirname}/dist/aspnet/ to ${destPath}`);
 
   await src(`${__dirname}/../dist/aspnet/**/*.*`)
     .pipe(replace('{{site.title}}', config.name))
